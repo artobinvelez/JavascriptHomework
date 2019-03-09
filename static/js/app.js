3a0ce60f39e7a11a8bd7de5d91411a8e090e440b
artgoblue <artobinvelez@gmail.com>
2019-03-09 09:50:25 -0600

    completed hw

---
+++ static/js/app.js
@@ -0,0 +1,49 @@
+// from data.js
+var tableData = data;
+
+var tbody = d3.select("tbody");
+
+// YOUR CODE HERE!
+data.forEach(function(ufoSightings) {
+    console.log(ufoSightings);
+    var row = tbody.append("tr");
+    Object.entries(ufoSightings).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  d3.selectAll("td").remove();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+
+  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+  filteredData.forEach(function(sighting) {
+    console.log(sighting);
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+});
